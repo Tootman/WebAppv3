69244aad1b41284507998737379ad76842c4f752
Dan Simmons <danieljsimmons@btinternet.com>
2018-08-27 14:31:18 +0100

    built

---
+++ build/precache-manifest.b08192c2274b7db72c4a592d3f48bb0b.js
@@ -0,0 +1,10 @@
+self.__precacheManifest = [
+  {
+    "revision": "b70cbf105eabf92d5b975f76710674a0",
+    "url": "index.html"
+  },
+  {
+    "revision": "0aea662f787ddf14c9fd",
+    "url": "app.bundle.js"
+  }
+];
