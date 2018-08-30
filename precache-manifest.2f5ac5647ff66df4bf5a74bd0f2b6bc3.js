d2b1881c6c75348c56b4e4aed99a0410754e07e8
Dan Simmons <danieljsimmons@btinternet.com>
2018-08-30 16:02:36 +0100

    Updates

---
+++ precache-manifest.2f5ac5647ff66df4bf5a74bd0f2b6bc3.js
@@ -0,0 +1,10 @@
+self.__precacheManifest = [
+  {
+    "revision": "458242bfae67707c67ae9d34edb0cd1c",
+    "url": "index.html"
+  },
+  {
+    "revision": "35d5b977f5dd2a5fa6ff",
+    "url": "app.bundle.js"
+  }
+];
---
+++ precache-manifest.fff1b1ea2ca214c32b0b5d6ac6f85b3b.js
@@ -0,0 +1,10 @@
+self.__precacheManifest = [
+  {
+    "revision": "59e23404221ce30db20610ad4c60181d",
+    "url": "index.html"
+  },
+  {
+    "revision": "dbf6e06d5cd2a52bd1d8",
+    "url": "app.bundle.js"
+  }
+];
