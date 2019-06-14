1aa3a603e458f0a9e44c20f47ab958f0cc29c96c
Dan Simmons <danieljsimmons@btinternet.com>
2019-06-14 15:37:32 +0100

    update

--- build/sw.js
+++ build/sw.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.4.1/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.a576573d95bb72169f3daafa3cc6ab90.js"
+  "precache-manifest.a24ed16df1a135f9ce9f41cf2ddb74bb.js"
 );
 
 workbox.skipWaiting();
